06675cdade32f6e7f859ba8d73982270134b3175
Mattias Rensmo <mattias@rensmo.se>
2023-11-06 17:45:37 +0100

    Gubbe o boxstavsväljare
    Bilden på gubbenligger på rätt plats.
    Tillfälligt ritar den ut en kroppsdel per klick pga test
    det händer på rad 3-13
    
    Nu fungerar det att klicka på bokstäverna
    OM de har class "letters__letter"
    Bara bokstänverna i possibleLetters är tillåtna att gissa på.
    Har man gisssat på dem en gång tas de bort ur possbleLetters.
    Så man kan bara gissa på samam bokstav en gång.

--- main.js
+++ main.js
@@ -1,11 +1,73 @@
+// TEST AREA BEGIN
+
+const bodyParts = document.querySelectorAll("#hangman__body-parts > *");
+bodyParts.forEach((part) => part.classList.add("hidden"));
+let part = 0;
+
+document.addEventListener("click", (e) => {
+ // console.log(e.target.innerText);
+
+ if (part == bodyParts.length) return;
+ bodyParts[part].classList.remove("hidden");
+ part++;
+});
+
+// TEST AREA END
+
 const dictionary = ["children", "roomy", "calculator", "reminiscent", "ubiquitous"];
 
 const word = randomWord(dictionary);
 let correctGuesses; // Här lägger vi in alla rätta bokstäver på rätt plats "a--ba"
-
+let possibleLetters = [
+ "a",
+ "b",
+ "c",
+ "d",
+ "e",
+ "f",
+ "g",
+ "h",
+ "i",
+ "j",
+ "k",
+ "l",
+ "m",
+ "n",
+ "o",
+ "p",
+ "q",
+ "r",
+ "s",
+ "t",
+ "u",
+ "v",
+ "w",
+ "x",
+ "y",
+ "z",
+ "å",
+ "ä",
+ "ö",
+];
 drawLetterLines(word);
 
-//Event listner för bokstav på tangentbord eller klick på själva bokstäverna (mattias) letterGuess(letter, word)
+//Event listener för letter på keyboard (mattias)
+document.addEventListener("keydown", (e) => {
+ const letter = e.key.toLocaleLowerCase();
+
+ //If its a allowed letter that has't been guessed yes
+
+ letterGuess(letter, word);
+});
+
+//Event listener for press the letter (mattias)
+const letterSection = document.querySelector(".letters");
+letterSection.addEventListener("click", (e) => {
+ if (e.target.classList.contains("letters__letter")) {
+  const letter = e.target.innerText.toLocaleLowerCase();
+  letterGuess(letter, word);
+ }
+});
 
 function randomWord(dictionary) {
  //Tar ett ord ur dictionary
@@ -16,6 +78,10 @@ function drawLetterLines(word) {
 }
 
 function letterGuess(letter, word) {
+ if (possibleLetters.includes(letter)) {
+  possibleLetters.splice(possibleLetters.indexOf(letter), 1); //Remove from allowed guesses
+  console.log("Guessed", letter);
+ }
  //Om bokstaven redan är gissad - gör inget
  //Om bokstaven finns i word kör correctGuess
  // Om bokstaven inte finns i word -  kör wrongguess
